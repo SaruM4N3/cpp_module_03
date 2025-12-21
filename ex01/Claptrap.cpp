9dde4fc34487aa275044d45fa0a652bad3e0ee47
SaruM4N3 <zakobajo@gmail.com>
2025-12-22 00:27:26 +0100

    Update color system to test ex00 and starting ex01

--- ex01/Claptrap.cpp
+++ ex01/Claptrap.cpp
@@ -6,7 +6,7 @@
 /*   By: zsonie <zsonie@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 01:14:32 by zsonie            #+#    #+#             */
-/*   Updated: 2025/12/22 00:21:28 by zsonie           ###   ########lyon.fr   */
+/*   Updated: 2025/12/22 00:15:25 by zsonie           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@
 Claptrap::Claptrap()
     : _name("DefaultClaptrap"), _hitpoint(10), _energypoint(10), _attackdmg(0)
 {
-    std::cout << CYAN << "Default constructor called on " << GREEN << this->_name << RESET << std::endl;
+    std::cout << CYAN << "Default constructor called on " << this->_name << RESET << std::endl;
 }
 
 Claptrap::Claptrap(std::string name)
     : _name(name), _hitpoint(10), _energypoint(10), _attackdmg(0)
 {
-    std::cout << CYAN << "Paramaterized constructor called on " << GREEN << this->_name << RESET << std::endl;
+    std::cout << CYAN << "Paramaterized constructor called on " << this->_name << RESET << std::endl;
 }
 
 Claptrap::Claptrap(const Claptrap &copy)
     : _name(copy._name), _hitpoint(copy._hitpoint), _energypoint(copy._energypoint), _attackdmg(copy._attackdmg)
 {
-    std::cout << CYAN << "Copy constructor called on " << GREEN << this->_name << RESET << std::endl;
+    std::cout << CYAN << "Copy constructor called on " << this->_name << RESET << std::endl;
 }
 
 Claptrap &Claptrap::operator=(const Claptrap &copy)
@@ -40,34 +40,32 @@ Claptrap &Claptrap::operator=(const Claptrap &copy)
         this->_energypoint = copy._energypoint;
         this->_attackdmg = copy._attackdmg;
     }
-    std::cout << CYAN << "Copy assignment operator called on " << GREEN << this->_name << RESET << std::endl;
+    std::cout << CYAN << "Copy assignment operator called on " << this->_name << RESET << std::endl;
     return *this;
 }
 
 Claptrap::~Claptrap()
 {
-    std::cout << RED << "Destructor called on " << GREEN << this->_name << RESET << std::endl;
+    std::cout << RED << "Destructor called on " << this->_name << RESET << std::endl;
 }
 
 void Claptrap::attack(const std::string &target)
 {
     if (this->_hitpoint == 0)
     {
-        std::cout << YELLOW << "Claptrap " << GREEN << this->_name
-                  << YELLOW << " is already dead! (currently 0hp)" << RESET << std::endl;
+        std::cout << YELLOW << "Claptrap " << this->_name
+                  << " is already dead! (currently 0hp)" << RESET << std::endl;
         return;
     }
     if (this->_energypoint == 0)
     {
-        std::cout << YELLOW << "Claptrap " << GREEN << this->_name
-                  << YELLOW << " doesnt have enough energy to attack" << RESET << std::endl;
+        std::cout << YELLOW << "Claptrap " << this->_name
+                  << " doesnt have enough energy to attack" << RESET << std::endl;
         return;
     }
     this->_energypoint--;
-    std::cout << YELLOW << "ClapTrap " << GREEN << this->_name
-              << YELLOW << " attacks " << GREEN << target
-              << YELLOW << ", causing " << CYAN << this->_attackdmg 
-              << YELLOW << " points of damage!"
+    std::cout << YELLOW << "ClapTrap " << this->_name << " attacks " << target
+              << ", causing " << this->_attackdmg << " points of damage!"
               << RESET << std::endl;
 }
 
@@ -75,35 +73,34 @@ void Claptrap::takeDamage(unsigned int amount)
 {
     if (this->_hitpoint == 0)
     {
-        std::cout << YELLOW << "Claptrap " << GREEN << this->_name
-                  << YELLOW << " is already dead! (currently 0hp)" << RESET << std::endl;
+        std::cout << YELLOW << "Claptrap " << this->_name
+                  << " is already dead! (currently 0hp)" << RESET << std::endl;
         return;
     }
     if (amount >= this->_hitpoint)
         this->_hitpoint = 0;
     else
         this->_hitpoint -= amount;
-    std::cout << YELLOW << "Claptrap " << GREEN << this->_name << " takes " << CYAN << amount
-              << YELLOW << " dmg! Current hit point is " << CYAN << this->_hitpoint << RESET << std::endl;
+    std::cout << YELLOW << "Claptrap " << this->_name << " takes " << amount
+              << " dmg! Current hit point is " << this->_hitpoint << RESET << std::endl;
 }
 
 void Claptrap::beRepaired(unsigned int amount)
 {
     if (this->_hitpoint == 0)
     {
-        std::cout << YELLOW << "Claptrap " << GREEN << this->_name
-                  << YELLOW << " is already dead! (currently 0hp)" << RESET << std::endl;
+        std::cout << YELLOW << "Claptrap " << this->_name
+                  << " is already dead! (currently 0hp)" << RESET << std::endl;
         return;
     }
     if (this->_energypoint == 0)
     {
-        std::cout << YELLOW << "Claptrap " << GREEN << this->_name
-                  << YELLOW << " doesnt have enough energy to repairs itself!" << RESET << std::endl;
+        std::cout << YELLOW << "Claptrap " << this->_name
+                  << " doesnt have enough energy to repairs itself!" << RESET << std::endl;
         return;
     }
     this->_energypoint--;
     this->_hitpoint += amount;
-    std::cout << YELLOW << "ClapTrap " << GREEN << this->_name
-              << YELLOW << " repairs itself for " << CYAN << amount
-              << YELLOW << " HP! Total HP: " << CYAN << this->_hitpoint << RESET << std::endl;
+    std::cout << YELLOW << "ClapTrap " << this->_name << " repairs itself for " << amount
+              << " HP! Total HP: " << this->_hitpoint << RESET << std::endl;
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zsonie <zsonie@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 23:50:00 by zsonie            #+#    #+#             */
-/*   Updated: 2025/12/22 00:26:35 by zsonie           ###   ########lyon.fr   */
+/*   Updated: 2025/12/22 00:04:19 by zsonie           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int main()
 {
     std::cout << GREEN << "=== Test 1: Basic Constructor ===" << RESET << std::endl;
-    Claptrap clap1("MONKEYTRAP");
+    Claptrap clap1("Bob");
 
     std::cout << GREEN << "=== Test 2: Default Constructor ===" << RESET << std::endl;
     Claptrap clap2;
@@ -25,11 +25,11 @@ int main()
     Claptrap clap3(clap1);
 
     std::cout << GREEN << "=== Test 4: Assignment Operator ===" << RESET << std::endl;
-    Claptrap clap4("MANTRAP");
+    Claptrap clap4("Alice");
     clap4 = clap1;
 
     std::cout << GREEN << "=== Test 5: Basic Attack ===" << RESET << std::endl;
-    clap1.attack("RANDOMTRAP");
+    clap1.attack("target dummy");
 
     std::cout << GREEN << "=== Test 6: Basic Repair ===" << RESET << std::endl;
     clap1.beRepaired(5);
@@ -38,32 +38,32 @@ int main()
     clap1.takeDamage(8);
 
     std::cout << GREEN << "=== Test 8: Multiple Actions (Energy Depletion) ===" << RESET << std::endl;
-    Claptrap energyTest("EnergyTrap");
+    Claptrap energyTest("EnergyBoy");
     for (int i = 0; i < 12; i++)
     {
         std::cout << "Action " << i + 1 << ": ";
-        energyTest.attack("EnergyVampireTrap");
+        energyTest.attack("enemy");
     }
 
     std::cout << GREEN << "=== Test 9: Death Test ===" << RESET << std::endl;
-    Claptrap deadTest("DeadManTrap");
+    Claptrap deadTest("DeadMan");
     deadTest.takeDamage(10);  // Should die
-    deadTest.attack("RandomTrap");  // Can't attack when dead
+    deadTest.attack("ghost");  // Can't attack when dead
     deadTest.beRepaired(5);    // Can't repair when dead
     deadTest.takeDamage(5);    // Already dead
 
     std::cout << GREEN << "=== Test 10: Repair After Damage ===" << RESET << std::endl;
-    Claptrap healTest("HealerTrap");
+    Claptrap healTest("Healer");
     healTest.takeDamage(7);
     healTest.beRepaired(3);
-    healTest.attack("targetTrap");
+    healTest.attack("target");
 
     std::cout << GREEN << "=== Test 11: Overkill Damage ===" << RESET << std::endl;
-    Claptrap overkill("VictimTrap");
+    Claptrap overkill("Victim");
     overkill.takeDamage(50);  // More damage than HP
 
     std::cout << GREEN << "=== Test 12: Energy Depletion with Repair ===" << RESET << std::endl;
-    Claptrap mixTest("MixTestTrap");
+    Claptrap mixTest("Mixer");
     mixTest.attack("enemy1");
     mixTest.beRepaired(2);
     mixTest.attack("enemy2");
