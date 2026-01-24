031ad26f0c6346a8c9d66d1a6dc192c80c4efeb5
SaruM4N3 <zakobajo@gmail.com>
2026-01-24 16:41:44 +0100

    ex01 ready to eval

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zsonie <zsonie@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 23:50:00 by zsonie            #+#    #+#             */
-/*   Updated: 2025/12/23 00:14:43 by zsonie           ###   ########lyon.fr   */
+/*   Updated: 2026/01/24 16:13:18 by zsonie           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,15 @@
 
 int main()
 {
-    std::cout << GREEN << "\n========== CLAPTRAP TESTS ==========" << RESET << std::endl;
-    std::cout << GREEN << "=== Test 1: Constructors and assignment ===" << RESET << std::endl;
+    if (!DEBUG_MODE)
+        std::cout << std::endl
+                  << RED << "FOR A FULL TEST PLS USE: make debug"
+                  << std::endl;
+    if (DEBUG_MODE)
+    {
+        std::cout << GREEN << "\n========== CLAPTRAP TESTS ==========" << RESET << std::endl;
+        std::cout << GREEN << "=== Test 1: Constructors and assignment ===" << RESET << std::endl;
+    }
     ClapTrap clap1("MONKEYTRAP");
     ClapTrap clap2;
     ClapTrap clap3(clap1);
@@ -32,7 +39,7 @@ int main()
     std::cout << std::endl;
     std::cout << GREEN << "=== Test 3: Multiple Actions (Energy Depletion) ===" << RESET << std::endl;
     ClapTrap energyTest("EnergyTrap");
-    std::cout << GREEN << "Silenting the 10 next attacks" << RESET << std::endl;
+    std::cout << YELLOW << "Silenting the 10 next attacks" << RESET << std::endl;
     std::streambuf *old = std::cout.rdbuf(0);
     for (int i = 0; i < 10; i++)
         energyTest.attack("EnergyVampireTrap");
@@ -57,9 +64,9 @@ int main()
     ClapTrap healTest("HealerTrap");
     healTest.takeDamage(7);
     healTest.beRepaired(3);
-    healTest.attack("targetTrap");
 
     std::cout << std::endl;
-    std::cout << GREEN << "=== Destructors will be called now ===" << RESET << std::endl;
+    if (DEBUG_MODE)
+        std::cout << GREEN << "=== Destructors will be called now ===" << RESET << std::endl;
     return 0;
 }
